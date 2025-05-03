ca1a7234efc523c886002ece4928f2a013d65516
holcan123 <rodrigolaratrevino@gmail.com>
2025-05-02 17:07:31 -0700

    git

--- Function_generator.py
+++ Function_generator.py
@@ -5,14 +5,14 @@ import random
 import matplotlib
 
 #lists 
-LC=random.randint(2,10)
-constantes=random.sample(range(1,10),LC)  #bug going on because "sample is larger than population or negative" this needs to be fixed af it happends whuen LC==10 the order in with those numbers are order it gives their power 
+LC=random.randint(1,10) #LC= leading coeficient 
+constantes=random.sample(range(1,11),LC)  #bug going on because this needs to be fixed af it happends whuen LC==10 the order in with those numbers are order it gives their power 
 polinomio=numpy.poly1d(constantes)             
 
 
 #display plotted line  
 
-X=numpy.linspace(-10,10,30) #defining linear spaces of point in x and then generating y values based on the polynommial class of numpy
+X=numpy.linspace(-10,10,100) #defining linear spaces of point in x and then generating y values based on the polynommial class of numpy
 Y=polinomio(X)
 
 
@@ -21,6 +21,7 @@ print(constantes)
 print(polinomio)
 print(X)
 print(Y)
+
 
 matplotlib.pyplot.figure(figsize=(10,10))
 matplotlib.pyplot.plot(X,Y)
