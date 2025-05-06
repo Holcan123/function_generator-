fcfa5d6fd1dfd4f49e4d6478106e1c4b08755ccc
holcan123 <rodrigolaratrevino@gmail.com>
2025-05-05 19:03:25 -0700

    adding interaccion with points

--- Function_generator.py
+++ Function_generator.py
@@ -4,9 +4,10 @@ import numpy
 import random
 import matplotlib
 
+
 #lists 
 LC=random.randint(1,10) #LC= leading coeficient 
-constantes=random.sample(range(1,11),LC)  #bug going on because this needs to be fixed af it happends whuen LC==10 the order in with those numbers are order it gives their power 
+constantes=random.sample(range(1,11),LC)  
 polinomio=numpy.poly1d(constantes)             
 
 
@@ -15,15 +16,23 @@ polinomio=numpy.poly1d(constantes)
 X=numpy.linspace(-10,10,100) #defining linear spaces of point in x and then generating y values based on the polynommial class of numpy
 Y=polinomio(X)
 
-
 print(LC)
 print(constantes)
 print(polinomio)
 print(X)
 print(Y)
 
+#making the points on the graph selectable
+def clickpoint(click):
+    identifier=click.ind[0]
+    x0 =X[identifier]
+    y0 =Y[identifier]
+ 
 
+
+#display plotted line 
 matplotlib.pyplot.figure(figsize=(10,10))
-matplotlib.pyplot.plot(X,Y)
+matplotlib.pyplot.plot(X,Y, marker= 'o',picker=3)
 matplotlib.pyplot.grid(True)
+matplotlib.pyplot.gcf().canvas.mpl_connect('pick_event', clickpoint)   # this is pure black magic credits to random smart indian guy! i think that Retrieves the selected point of the current function and records its value when you click on it and does what the you tell the function to do! .
 matplotlib.pyplot.show()
